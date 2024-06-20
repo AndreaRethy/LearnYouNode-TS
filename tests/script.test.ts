8333ae6a00abdb59ffff9a284ac92a0ee2a81512
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-06-20 13:43:04 +0200

    Do the first exercise: Hello World

--- tests/script.test.ts
+++ tests/script.test.ts
@@ -1,6 +1,12 @@
+import { sayHello } from "../src/hello-world"
+
 // tests/sample.test.ts
-describe('Sample Test', () => {
+describe('Should log Hello World', () => {
     it('should return true', () => {
-      expect(true).toBe(true);
+      console.log = jest.fn();
+      const hello: string = "HELLO WORLD";
+      sayHello(hello);
+
+      expect(console.log).toHaveBeenCalledWith("HELLO WORLD")
     });
   });
